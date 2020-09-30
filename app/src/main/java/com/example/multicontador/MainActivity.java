bc583eb4f92cefd0a057c81ab1b41d437b6b2a43
Oscar Vazquez Mier <ovazquez@elpuig.xeill.net>
2020-09-30 19:15:21 +0200

    -- versión final

--- app/src/main/java/com/example/multicontador/MainActivity.java
+++ app/src/main/java/com/example/multicontador/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
     int contador2;
     int contador3;
     int contador4;
+    int contadorAll;
 
     ActivityMainBinding binding;
 
@@ -25,33 +26,99 @@ public class MainActivity extends AppCompatActivity {
         binding.sum1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                contadorAll++;
                 contador1++;
-                binding.contador1.setText(contador1);
+                binding.contador1.setText(String.valueOf(contador1));
+                binding.contadorAll.setText(String.valueOf(contadorAll));
+
             }
       });
 
         binding.sum2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                contadorAll++;
                 contador2++;
-                binding.contador2.setText(contador2);
+                binding.contador2.setText(String.valueOf(contador2));
+                binding.contadorAll.setText(String.valueOf(contadorAll));
             }
         });
 
         binding.sum3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                contadorAll++;
                 contador3++;
-                binding.sum3.setText(String.valueOf(contador3));;
+                binding.contador3.setText(String.valueOf(contador3));
+                binding.contadorAll.setText(String.valueOf(contadorAll));
             }
         });
 
         binding.sum4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                contadorAll++;
                 contador4++;
+                binding.contador4.setText(String.valueOf(contador4));
+                binding.contadorAll.setText(String.valueOf(contadorAll));
+            }
+        });
+
+        binding.reset1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                contadorAll = contadorAll - contador1;
+                binding.contadorAll.setText(String.valueOf(contadorAll));
+                contador1 = 0;
+                binding.contador1.setText(String.valueOf(contador1));
+            }
+        });
+
+        binding.reset2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                contadorAll = contadorAll - contador2;
+                binding.contadorAll.setText(String.valueOf(contadorAll));
+                contador2 = 0;
+                binding.contador2.setText(String.valueOf(contador2));
+            }
+        });
+
+        binding.reset3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                contadorAll = contadorAll - contador3;
+                binding.contadorAll.setText(String.valueOf(contadorAll));
+                contador3 = 0;
+                binding.contador3.setText(String.valueOf(contador3));
+            }
+        });
+
+        binding.reset4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                contadorAll = contadorAll - contador4;
+                binding.contadorAll.setText(String.valueOf(contadorAll));
+                contador4 = 0;
                 binding.contador4.setText(String.valueOf(contador4));
             }
         });
+
+        binding.resetAll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                contadorAll = 0;
+                binding.contadorAll.setText(String.valueOf(contadorAll));
+                contador1 = 0;
+                binding.contador1.setText(String.valueOf(contador1));
+                contador2 = 0;
+                binding.contador2.setText(String.valueOf(contador2));
+                contador3 = 0;
+                binding.contador3.setText(String.valueOf(contador3));
+                contador4 = 0;
+                binding.contador4.setText(String.valueOf(contador4));
+            }
+        });
+
     }
 }
